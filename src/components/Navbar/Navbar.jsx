f9c7a0b9a5d1c8aebc59853ebdbdc070f8e51408
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-03-14 01:08:31 +0200

    add style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,24 +7,24 @@ import UserMenu from './UserMenu/UserMenu';
 import css from './navbar.module.css';
 import { isUserLogin } from '../../redux/auth/auth-selectors';
 
-import items from './items';
+// import items from './items';
 
 const Navbar = () => {
   const isLogin = useSelector(isUserLogin);
-
-  const elements = items.map(({ id, text, link }) => (
-    <li key={id}>
-      <NavLink className={css.link} to={link}>
-        {text}
-      </NavLink>
-    </li>
-  ));
-
   return (
     <header className={css.header}>
       {/* <NavLink to="/">Logo</NavLink> */}
 
-      <ul className={css.menu}>{elements}</ul>
+      <ul className={css.menu}>
+        <NavLink className={css.link} to="/">
+          <p>Home</p>
+        </NavLink>
+        {isLogin && (
+          <NavLink className={css.link} to="/my-contacts">
+            <p>Contacts</p>
+          </NavLink>
+        )}
+      </ul>
 
       {!isLogin && <NavbarAuth />}
       {isLogin && <UserMenu />}
